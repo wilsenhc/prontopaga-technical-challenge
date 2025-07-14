fea7199b873fdc0c2ea32a3d0153f91023526585
Wilsen Hernández <13445515+wilsenhc@users.noreply.github.com>
2025-07-14 10:26:34 -0400

    Publish sail + add phpmyadmin

---
+++ docker/phpmyadmin/config.user.inc.php
@@ -0,0 +1,13 @@
+<?php
+
+// Defines whether or not to link with main panel by highlighting the current database or table.
+// https://docs.phpmyadmin.net/en/latest/config.html#cfg_NavigationLinkWithMainPanel
+$cfg['NavigationLinkWithMainPanel'] = false;
+
+// The number of items (tables, columns, indexes) that can be displayed on each page of the navigation tree.
+// https://docs.phpmyadmin.net/en/latest/config.html#cfg_MaxNavigationItems
+$cfg['MaxNavigationItems'] = 100;
+
+// Defines whether to group the databases based on a common prefix in their name
+// https://docs.phpmyadmin.net/en/latest/config.html#cfg_NavigationTreeEnableGrouping
+$cfg['NavigationTreeEnableGrouping'] = false;
